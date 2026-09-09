6aba4d01b7d54b352d742e2d3556cec742051cb5
Luciano Vieira <byskopf@gmail.com>
2026-09-09 00:09:00 -0300

    Add Central Administrativa PWA config

---
+++ admin/app-config.js
@@ -0,0 +1,5 @@
+/* Configuração única da Central Administrativa CHECK-SE / CHECK-LT. */
+var CHECK_ADMIN_CONFIG = Object.freeze({
+  version: '1.0.0',
+  appUrl: 'https://script.google.com/macros/s/AKfycbx7ReYi1OoXTvO2e-FLXczg-Ube0WZcjJSWe8HGlWbQtUof1F-1dOUQ-kqrwZQPjb6D/exec?central=admin'
+});
