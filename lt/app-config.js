ba5b2c043c6e5c418afe879dabcbaf8bf0caec88
Luciano Vieira <byskopf@gmail.com>
2026-09-08 22:41:49 -0300

    Adiciona configuração do PWA CHECK-LT

---
+++ lt/app-config.js
@@ -0,0 +1,5 @@
+/* Configuração única do portal CHECK-LT. */
+var CHECK_LT_CONFIG = Object.freeze({
+  version: '1.0.0',
+  appUrl: 'https://script.google.com/macros/s/AKfycbyrL8EqMnFFHwq4VqhjvFJinaQYMsxobJ2bv93FYR5-HqRVjY9BdUbbIHj8iOjY-Sc/exec'
+});
