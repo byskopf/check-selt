453e2ad7e78a640851c31a5b5e6842d5ef6fd77a
Luciano Vieira <byskopf@gmail.com>
2026-09-10 03:43:07 -0300

    Força atualização do PWA do CHECK-LT

--- lt/app-config.js
+++ lt/app-config.js
@@ -1,5 +1,5 @@
 /* Configuração única do portal CHECK-LT. */
 var CHECK_LT_CONFIG = Object.freeze({
-  version: '1.0.0',
+  version: '1.0.1',
   appUrl: 'https://script.google.com/macros/s/AKfycbzo_WN_PzoRhS-LhV070vmE8GDr1vJX9qEa1iqxADe6kVhNZa968olZLVVDAtObmvE/exec'
 });
