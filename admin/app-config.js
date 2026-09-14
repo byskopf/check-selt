1f1dbd87a71d735fd2a2d5ea8302aa0b730d38bf
Luciano Vieira <byskopf@gmail.com>
2026-09-13 22:57:34 -0300

    Atualiza link da Central Administrativa

--- admin/app-config.js
+++ admin/app-config.js
@@ -1,5 +1,5 @@
 /* Configuração única da Central Administrativa CHECK-SE / CHECK-LT. */
 var CHECK_ADMIN_CONFIG = Object.freeze({
-  version: '1.0.0',
-  appUrl: 'https://script.google.com/macros/s/AKfycbx7ReYi1OoXTvO2e-FLXczg-Ube0WZcjJSWe8HGlWbQtUof1F-1dOUQ-kqrwZQPjb6D/exec?central=admin'
+  version: '1.0.1',
+  appUrl: 'https://script.google.com/macros/s/AKfycbxUC3NNE_SPW9NSzkgGXAbJC3kkX9XJtYCpY5CC0kZWFqtzUKDV99Vx0qoNtPoXtcxo/exec?central=admin'
 });
