027b2e06d3e3a913f53ae0b93b54e57c525da802
Luciano Vieira <byskopf@gmail.com>
2026-09-15 13:15:56 -0300

    chore(admin): atualiza versão para 1.1.1

--- admin/app-config.js
+++ admin/app-config.js
@@ -1,5 +1,5 @@
 /* Configuração única da Central Administrativa CHECK-SE / CHECK-LT. */
 var CHECK_ADMIN_CONFIG = Object.freeze({
-  version: '1.1.0',
+  version: '1.1.1',
   appUrl: 'https://script.google.com/macros/s/AKfycbxUC3NNE_SPW9NSzkgGXAbJC3kkX9XJtYCpY5CC0kZWFqtzUKDV99Vx0qoNtPoXtcxo/exec?central=admin'
 });
