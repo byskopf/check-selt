a5c8c9b92b84de90a40732867c4df4f76662343c
Luciano Vieira <byskopf@gmail.com>
2026-09-15 13:54:08 -0300

    chore(admin): separa versão dos ícones

--- admin/app-config.js
+++ admin/app-config.js
@@ -1,5 +1,6 @@
 /* Configuração única da Central Administrativa CHECK-SE / CHECK-LT. */
 var CHECK_ADMIN_CONFIG = Object.freeze({
-  version: '1.1.1',
+  version: '1.1.2',
+  iconVersion: '20260915',
   appUrl: 'https://script.google.com/macros/s/AKfycbxUC3NNE_SPW9NSzkgGXAbJC3kkX9XJtYCpY5CC0kZWFqtzUKDV99Vx0qoNtPoXtcxo/exec?central=admin'
 });
